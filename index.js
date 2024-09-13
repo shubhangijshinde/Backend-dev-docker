f120bf22655b8289a9e29d2ceba4fe3101e2cb68
shubhangijshinde <shubhangishinde1089@gmail.com>
2024-09-13 15:04:30 -0700

    Initial commit

--- index.js
+++ index.js
@@ -54,4 +54,27 @@ app.put("/api/genres/:id", (req, res) => {
     res.send(genres);
 });
 
-app.listen(3000, () => console.log("Listening on port 3000.."));
+const router = express.Router();
+
+const path = __dirname + '/Views/';
+const port = 8080;
+
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
+});
+
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks', function(req,res){
+  res.sendFile(path + 'sharks.html');
+});
+
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 8080!')
+})
